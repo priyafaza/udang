2be7f03ac67ca28976fa20099104f8823e8d61b5
ijlik <ijlik.k5s@gmail.com>
2022-06-16 13:33:46 +0700

    Update payment proof add validation

--- resources/views/admin/report/index.blade.php
+++ resources/views/admin/report/index.blade.php
@@ -32,8 +32,8 @@
                     <!-- /.card-header -->
                     <div class="card-body">
                         <form action="" method="GET">
-                            <input type="date" name="start_date">
-                            <input type="date" name="end_date">
+                            <input type="datetime-local" name="start_date">
+                            <input type="datetime-local" name="end_date">
                             <button type="submit">Filter</button>
                         </form>
                         <table id="example1" class="table table-bordered table-striped">
